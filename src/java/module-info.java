7e79c01795147f32ca7fe2e6133cb43bb6399b60
hodeiez <hodeinaiz@hotmail.com>
2020-11-30 13:38:48 +0100

    Initial commit

---
+++ src/java/module-info.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Hodei Eceiza
+ * Date: 11/26/2020
+ * Time: 20:25
+ * Project: JavaFxMaven
+ * Copyright: MIT
+ */module BlackJackOOAD {
+     requires javafx.controls;
+     requires javafx.fxml;
+     requires javafx.graphics;
+     opens BlackJack;
+}
