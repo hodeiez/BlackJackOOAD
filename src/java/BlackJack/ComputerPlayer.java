121b9cbbaa9109470243ebf45a62d3523477c363
Lukas Aronsson <Lukas.Aronsson@protonmail.com>
2020-12-01 16:32:47 +0100

    Started LukasBranch, Created the classes IHasCards, Dealer, Player & ComputerPlayer.
    Signed-off-by: Lukas Aronsson <Lukas.Aronsson@protonmail.com>

---
+++ src/java/BlackJack/ComputerPlayer.java
@@ -0,0 +1,11 @@
+package BlackJack;
+
+/**
+ * Created by Lukas Aronsson
+ * Date: 01/12/2020
+ * Time: 16:23
+ * Project: BlackJackOOAD
+ * Copyright: MIT
+ **/
+public class ComputerPlayer {
+}
---
+++ src/java/BlackJack/IHasCards.java
@@ -0,0 +1,11 @@
+package BlackJack;
+
+/**
+ * Created by Lukas Aronsson
+ * Date: 01/12/2020
+ * Time: 16:22
+ * Project: BlackJackOOAD
+ * Copyright: MIT
+ **/
+public class IHasCards {
+}
