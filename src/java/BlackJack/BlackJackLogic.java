81861d814e1d4b8919f9ddfc95de8ac0ee06bd1d
Jesper Jansson <jesper.jansson@yh.nackademin.se>
2020-12-03 13:49:51 +0100

    Börjar bli grötigt utan de saknade klasserna så jag mergar in för att kunna få tag i dem xD

--- src/java/BlackJack/BlackJackLogic.java
+++ src/java/BlackJack/BlackJackLogic.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class BlackJackLogic {
     private List<Player> players = new ArrayList<>();
     private Dealer dealer1 = new Dealer;
-    private Deck d1 = new Deck;
+    private Deck deck1 = new Deck(); //lugnt om deck1 är tom här, isåfall så fixxas deet ihop ny lek i början av första rundan
     private Player activePlayer = new Player;
 
 
@@ -16,21 +16,44 @@ public class BlackJackLogic {
 
 
     private void setUpGame(){
-        Player copyMe = new Player;
-        Player.setBalance(1000);
-        
+        players.add(new Player);
+        players.add(new Player);
+        players.add(activePlayer);
+        setStartingBalance(1000);
+        playRound();
+
     }
 
     private void isItTimeToShuffle(){
-        if (d1.cardList.size()>=30) d1 = new Deck(6);
-        Collections.shuffle(d1.cardList);
+        if (deck1.cardList.size()<=30) deck1 = new Deck(6);
+        Collections.shuffle(deck1.cardList);
     }
+
+    private void playRound(){
+       while (true){
+        isItTimeToShuffle();
+        dealHands():
+
+    }}
 
     private void isPlayerBroke(Player player){
         if(player.getBalace <=0){
             player.setBroke(true);
 
         }
+    }
+    private void setStartingBalance(int startCash){
+        for (Player p: players) {
+            p.setBalance(startCash);
+        }
+    }
+    private void dealHands(){
+        for (Player p: players) {
+            p.hand.add(deck1.drawCard());
+            p.hand.add(deck1.drawCard());
+
+        }
+        dealer1.hand.add(deck1.drawCard());
     }
 
 }
