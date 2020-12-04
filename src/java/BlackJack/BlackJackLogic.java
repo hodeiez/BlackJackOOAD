ee818eea4ee89a21b1b3497dfe00da2269ee5a4d
Jesper Jansson <jesper.jansson@yh.nackademin.se>
2020-12-04 13:01:15 +0100

    todo- skriva isGameOver()

--- src/java/BlackJack/BlackJackLogic.java
+++ src/java/BlackJack/BlackJackLogic.java
@@ -1,5 +1,6 @@
 package BlackJack;
 
+import javax.swing.*;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
@@ -12,10 +13,7 @@ public class BlackJackLogic {
     private Player activePlayer = new Player;
 
 
-
-
-
-    private void setUpGame(){
+    private void setUpGame() {
         players.add(activePlayer);
         players.add(new Player);
         players.add(new Player);
@@ -24,37 +22,40 @@ public class BlackJackLogic {
 
     }
 
-    private void isItTimeToShuffle(){
-        if (deck1.cardList.size()<=30) deck1 = new Deck(6);
+    private void isItTimeToShuffle() {
+        if (deck1.cardList.size() <= 30) deck1 = new Deck(6);
         Collections.shuffle(deck1.cardList);
     }
 
-    private void playRound(){
-       while (true){
-        isItTimeToShuffle();
-        dealHands();
-        dealer1.hand.get(0).isFaceUp(true);
-        humanPlayerTurn();
-        computerPlayerTurn(players.get(1));
-        computerPlayerTurn(players.get(2));
-        dealerTurn();
-        isGameOver();
+    private void playRound() {
+        while (true) {
+            isItTimeToShuffle();
+            dealHands();
+            dealer1.hand.get(0).isFaceUp(true);
+            humanPlayerTurn();
+            computerPlayerTurn(players.get(1));
+            computerPlayerTurn(players.get(2));
+            dealerTurn();
+            isGameOver();
 
-    }}
+        }
+    }
 
-    private void isPlayerBroke(Player player){
-        if(player.getBalace <=0){
+    private void isPlayerBroke(Player player) {
+        if (player.getBalace <= 0) {
             player.setBroke(true);
 
         }
     }
-    private void setStartingBalance(int startCash){
-        for (Player p: players) {
+
+    private void setStartingBalance(int startCash) {
+        for (Player p : players) {
             p.setBalance(startCash);
         }
     }
-    private void dealHands(){
-        for (Player p: players) {
+
+    private void dealHands() {
+        for (Player p : players) {
             p.hand.add(deck1.drawCard());
             p.hand.add(deck1.drawCard());
 
@@ -62,66 +63,83 @@ public class BlackJackLogic {
         dealer1.hand.add(deck1.drawCard());
         dealer1.hand.add(deck1.drawCard());
     }
-private void humanPlayerTurn(){
+
+    private void humanPlayerTurn() {
         boolean hit;
-        while (true){
+        while (true) {
             System.out.println(activePlayer.getHandValue());
             //Input från användaren Hit/Stay
-            if(hit = true){
+            if (hit = true) {
                 activePlayer.hand.add(deck1.drawCard());
-            }else break;
+            } else break;
 
         }
-}
-    private void dealerTurn(){
-        dealer1.hand.get(1).isFaceUp(true);
-        while (dealer1.getHandValue < 21){
-            //Todo- Skriv vad dealern ska hitta på här :)
-        }
-
-
     }
-    private void computerPlayerTurn(Player player){
-        while (player.getHandValue() <=21){
-            player.hand.add(deck1.drawCard());
-            if (player.getHandValue() <=16){
-                break;
+
+    private void dealerTurn() {
+        dealer1.hand.get(1).isFaceUp(true);
+        while (dealer1.getHandValue() < 21) {
+            if (dealer1.getHandValue() <= activePlayer.getHandValue()) {
+                dealer1.hand.add(deck1.drawCard);
+            }
+            if (dealerTotal > 21) {
+                System.out.println("Dealern är bust! Du vinner.");
+
+            } else if (dealerTotal > playerTotal) {
+                System.out.println("Dealern vinner!");
+
+            } else if (dealerTotal == playerTotal) {
+                System.out.println("Oavgjort!");
+
+            } else if (dealerTotal < playerTotal) {
+                System.out.println("Du vinner!");
+
             }
 
-        }
-    }
 
-    //Den här metoden räknar ut värdet av en spelares hand. Den tar till vara på om ifall en spelare har Ess
-    //Vanliga kort har värde 1-13 ess har värde 0, eller -1 beroende på om en vill att det ska vara värt
-    //11 eller 1 poäng.
-    private int getHandValue() {
-        int lenght = hand.size();
-        int total = 0;
-        for (int i = 0; i < lenght; i++) {
-            Card temp = (Card) hand.get(i);
-            if (temp.getNumber() >= 10) total = total + 10;//Alla kort från tio och upp är värda 10
-            if (temp.getNumber() < 10 && temp.getNumber() > 0) total = total + temp.getNumber();//kortets värde läggs till totalen
-            if (temp.getNumber() == 0) total = total + 11;//Ess kan vara värda 11
-            if (temp.getNumber() == -1) total = total + 1;//Ess kan vara värda 1
-            if (total > 21) {
-                //Om vi har mer än 21 kollar denna loop igenom ifall vi har Ess, om vi har det sätter vi ässet till att
-                //vara värt ett istället för elva och börjar om räkningen.
-                for (int j = 0; j < lenght; j++) {
-                    Card temp2 = (Card) hand.get(j);
-                    if (temp2.getNumber() == 0) {
-                        hand.set(j, new Card(temp2.getSuit(), -1));
-                        i = -1;
-                        total = 0;
-                        j = lenght;
+        }}
+        private void computerPlayerTurn (Player player){
+            while (player.getHandValue() <= 21) {
+                player.hand.add(deck1.drawCard());
+                if (player.getHandValue() <= 16) {
+                    break;
+                }
+
+            }
+        }
+
+        //Den här metoden räknar ut värdet av en spelares hand. Den tar till vara på om ifall en spelare har Ess
+        //Vanliga kort har värde 1-13 ess har värde 0, eller -1 beroende på om en vill att det ska vara värt
+        //11 eller 1 poäng.
+        private int getHandValue () {
+            int lenght = hand.size();
+            int total = 0;
+            for (int i = 0; i < lenght; i++) {
+                Card temp = (Card) hand.get(i);
+                if (temp.getNumber() >= 10) total = total + 10;//Alla kort från tio och upp är värda 10
+                if (temp.getNumber() < 10 && temp.getNumber() > 1)
+                    total = total + temp.getNumber();//kortets värde läggs till totalen
+                if (temp.getNumber() == 0) total = total + 11;//Ess kan vara värda 11
+                if (temp.getNumber() == -1) total = total + 1;//Ess kan vara värda 1
+                if (total > 21) {
+                    //Om vi har mer än 21 kollar denna loop igenom ifall vi har Ess, om vi har det sätter vi ässet till att
+                    //vara värt ett istället för elva och börjar om räkningen.
+                    for (int j = 0; j < lenght; j++) {
+                        Card temp2 = (Card) hand.get(j);
+                        if (temp2.getNumber() == 0) {
+                            hand.set(j, new Card(temp2.getSuit(), -1));
+                            i = -1;
+                            total = 0;
+                            j = lenght;
+                        }
                     }
                 }
             }
+            for (int i = 0; i < hand.size(); i++) { //Skriver ut korten till konsolen.
+                System.out.println(hand.get(i));
+            }
+            System.out.println("Totalt värde: " + total); // Skriver ut handens totala värde.
+            return total;
         }
-        for (int i = 0; i < hand.size(); i++) { //Skriver ut korten till konsolen.
-            System.out.println(hand.get(i));
-        }
-        System.out.println("Totalt värde: " + total); // Skriver ut handens totala värde.
-        return total;
-    }
 
-}
+    }
