a1e56d6d3e2d1d4da6b20b53690956523d03b441
Jesper Jansson <jesper.jansson@yh.nackademin.se>
2020-12-04 14:55:12 +0100

    Ändrat så att koden är körbar.

--- src/java/BlackJack/BlackJackLogic.java
+++ src/java/BlackJack/BlackJackLogic.java
@@ -1,9 +1,7 @@
 package BlackJack;
 
-import javax.swing.*;
+
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 
 public class BlackJackLogic {
@@ -33,19 +31,19 @@ public class BlackJackLogic {
         while (true) {
             isItTimeToShuffle();
             dealHands();
-            dealer1.hand.get(0).isFaceUp(true);
+            dealer1.hand.get(0).setFaceUp(true);
             humanPlayerTurn();
             computerPlayerTurn(players.get(1));
             computerPlayerTurn(players.get(2));
             dealerTurn();
-            isGameOver();
+//            isGameOver();
 
         }
     }
 
     private void isPlayerBroke(Player player) {
         if (player.getBalance() <= 0) {
-            player.setBroke(true);
+//            player.setBroke(true);
 
         }
     }
@@ -79,7 +77,7 @@ public class BlackJackLogic {
     }
 
     private void dealerTurn() {
-        dealer1.hand.get(1).isFaceUp(true);
+        dealer1.hand.get(1).setFaceUp(true);
         while (dealer1.getHandValue() < 21) {
             if (dealer1.getHandValue() <= activePlayer.getHandValue()) {
                 dealer1.hand.add(deck1.drawCard());
@@ -112,38 +110,6 @@ public class BlackJackLogic {
         }
     }
 
-    //Den här metoden räknar ut värdet av en spelares hand. Den tar till vara på om ifall en spelare har Ess
-    //Vanliga kort har värde 1-13 ess har värde 0, eller -1 beroende på om en vill att det ska vara värt
-    //11 eller 1 poäng.
-    private int getHandValue() {
-        int lenght = hand.size();
-        int total = 0;
-        for (int i = 0; i < lenght; i++) {
-            Card temp = (Card) hand.get(i);
-            if (temp.getNumber() >= 10) total = total + 10;//Alla kort från tio och upp är värda 10
-            if (temp.getNumber() < 10 && temp.getNumber() > 1)
-                total = total + temp.getNumber();//kortets värde läggs till totalen
-            if (temp.getNumber() == 0) total = total + 11;//Ess kan vara värda 11
-            if (temp.getNumber() == -1) total = total + 1;//Ess kan vara värda 1
-            if (total > 21) {
-                //Om vi har mer än 21 kollar denna loop igenom ifall vi har Ess, om vi har det sätter vi ässet till att
-                //vara värt ett istället för elva och börjar om räkningen.
-                for (int j = 0; j < lenght; j++) {
-                    Card temp2 = (Card) hand.get(j);
-                    if (temp2.getNumber() == 0) {
-                        hand.set(j, new Card(temp2.getSuit(), -1));
-                        i = -1;
-                        total = 0;
-                        j = lenght;
-                    }
-                }
-            }
-        }
-        for (int i = 0; i < hand.size(); i++) { //Skriver ut korten till konsolen.
-            System.out.println(hand.get(i));
-        }
-        System.out.println("Totalt värde: " + total); // Skriver ut handens totala värde.
-        return total;
-    }
+
 
 }
