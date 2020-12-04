9995b7f8d2af56c093626e8f570aa6b42c7e9f03
Katri <suntribe2020@gmail.com>
2020-12-04 14:15:44 +0100

    Created Deck and Card classes

---
+++ src/java/BlackJack/Card.java
@@ -0,0 +1,36 @@
+package BlackJack;
+
+public class Card {
+
+    private final Suit suit;
+    private final int rank;
+    private boolean faceUp;
+
+    public Card(Suit suit, int rank) {
+        this.rank = rank;
+        this.suit = suit;
+    }
+
+    public Suit getSuit() {
+        return suit;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public boolean isFaceUp() {
+        return faceUp;
+    }
+
+    public void setFaceUp(boolean faceUp) {
+        this.faceUp = faceUp;
+    }
+
+    public enum Suit {
+        HEARTS,
+        SPADES,
+        CLUBS,
+        DIAMONDS
+    }
+}
