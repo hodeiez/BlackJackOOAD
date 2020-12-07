11916781edfa7bd65dc72ce86b842613d7969d51
kdhedman <kenth.david.hedman@gmail.com>
2020-12-07 21:25:20 +0100

    IHasCards - Ändrade getHandValue till en variant med färre ifsatser samt kopierar värdena från Card istället för att använda Card.
    Deck - ändrade drawCard() till enradaren den var från början.
    
    GameBoardController - La till en ButtonListener för att lyssna på booleanvärde om knappar ska vara aktiva eller inte.
    
    BlackJackLogic -
    Dealer drar nu alltid ett kort till och tråden sover sen i en sekund mellan dragningar för att få mer naturligt flyt i spelet.
    Dealer slutar dra vid 16.
    Lagt till sleep på några ms efter att spelare eller dealer drar kort då det missades av någon anledning, osäker på varför men detta fungerade och 3 ms märks inte.
    Ändrade ordning när kort räknas för spelar till slutet av metoden.
    Lagt till en BooleanProperty disableButtons för att kunna aktivera knapparna enbart när vi väntar input från användare.

--- src/java/BlackJack/GameBoardController.java
+++ src/java/BlackJack/GameBoardController.java
@@ -1,6 +1,7 @@
 package BlackJack;
 
-import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -8,11 +9,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
-
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
 
 /**
  * Created by Hodei Eceiza
@@ -29,58 +26,64 @@ public class GameBoardController {
     public Button end;
     public Label balance;
     public HBox dealerBox;
+    public HBox dealerbackground;
     public HBox activePlayer;
     public HBox player2;
     public HBox player3;
     public Label handValue;
     @FXML
     private AnchorPane gameBoardPane;
-  //  private ModelTest modelTest;
-   private BlackJackLogic modelTest;
+    //  private ModelTest modelTest;
+    private BlackJackLogic modelTest;
     private Rectangle rect = new Rectangle();
-/*
-    public GameBoardController(ModelTest modelTest) {
-        this.modelTest = modelTest;
-    }
 
- */
-     public GameBoardController(BlackJackLogic modelTest) {
+    /*
+        public GameBoardController(ModelTest modelTest) {
+            this.modelTest = modelTest;
+        }
+
+     */
+    public GameBoardController(BlackJackLogic modelTest) {
 
         this.modelTest = modelTest;
     }
 
 
     public void initialize() {
-setListener(modelTest.activePlayer.hand,activePlayer);
+        setListener(modelTest.activePlayer.hand, activePlayer);
 
-setListener(modelTest.dealer1.hand,dealerBox);
+        setListener(modelTest.dealer1.hand, dealerBox);
+
+        setButtonListener();
+
 //Listens changes of the observableList
 
 
 //balance its binded, should do a double bind? or call method from logic to change the balance?
-       // balance.textProperty().bind(modelTest.balanceProperty());
+        // balance.textProperty().bind(modelTest.balanceProperty());
 //        handValue.textProperty().bind(modelTest.handValueProperty());
 
 
         //using buttons for test
         //test for changeFaceUp
-       // stay.setOnAction(e-> modelTest.activePlayerHandArr.get(0).changeFace());
+        // stay.setOnAction(e-> modelTest.activePlayerHandArr.get(0).changeFace());
 
-       //testing hit
+        //testing hit
         //hit.setOnAction(e->modelTest.hitListener());
-        hit.setOnAction(e->BlackJackLogic.actionQueue.add(1));
+        hit.setOnAction(e -> BlackJackLogic.actionQueue.add(1));
         end.setOnAction(e -> player2.getChildren().add(new CardGraph("clubs", "ace", true)));
-        stay.setOnAction(e->BlackJackLogic.actionQueue.add(0));
+        stay.setOnAction(e -> BlackJackLogic.actionQueue.add(0));
 
     }
-    public void setListener(ObservableList<Card> observable, HBox playerBox){
+
+    public void setListener(ObservableList<Card> observable, HBox playerBox) {
         observable.addListener((ListChangeListener<Card>) change -> {
             while (change.next()) {
                 if (change.wasAdded()) {
-                    CardGraph c= cardToGraph(change.getAddedSubList().get(0));
-                  //  CardGraph c=change.getAddedSubList().get(0);
-                    if(observable.size()>1)
-                        c.setTranslateX(-(50*(observable.size()-1)));//this has to be simplified
+                    CardGraph c = cardToGraph(change.getAddedSubList().get(0));
+                    //  CardGraph c=change.getAddedSubList().get(0);
+                    if (observable.size() > 1)
+                        c.setTranslateX(-(50 * (observable.size() - 1)));//this has to be simplified
                     playerBox.getChildren().add(c);
 
                 } else if (change.wasRemoved()) {
@@ -98,17 +101,27 @@ setListener(modelTest.dealer1.hand,dealerBox);
         });
     }
 
-    public CardGraph cardToGraph(Card card){
-        String rank=String.valueOf(card.getRank());
-        String suit=String.valueOf(card.getSuit()).toLowerCase();
-        switch (rank){
-            case "0"->rank="ace";
-            case "1"->rank="ace";
-            case "11"-> rank="jack";
-            case "12"->rank="queen";
-            case "13"->rank="king";
-            default -> rank=rank;
+    public void setButtonListener(){
+        modelTest.disableButtons.addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+                hit.setDisable(newValue);
+                stay.setDisable(newValue);
+            }
+        });
+    }
+
+    public CardGraph cardToGraph(Card card) {
+        String rank = String.valueOf(card.getRank());
+        String suit = String.valueOf(card.getSuit()).toLowerCase();
+        switch (rank) {
+            case "0" -> rank = "ace";
+            case "1" -> rank = "ace";
+            case "11" -> rank = "jack";
+            case "12" -> rank = "queen";
+            case "13" -> rank = "king";
+            default -> rank = rank;
         }
-        return new CardGraph(suit,rank,true);
+        return new CardGraph(suit, rank, true);
     }
 }
